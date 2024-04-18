d8de8e9ef3f5073dc8d6e86004130b1a5e6c5b3c
Fatima Alubaidi <fatima@Fatimas-MacBook-Air.local>
2024-04-18 12:43:02 +0100

    basic

--- java/src/C22305656/FA.java
+++ java/src/C22305656/FA.java
@@ -1,5 +1,43 @@
 package C22305656;
 
-public class FA {
-    // test1
+import processing.core.PApplet;
+import ddf.minim.*;
+import ddf.minim.analysis.*;
+
+public class FA extends PApplet {
+    private Minim minim;
+    private AudioPlayer player;
+    private FFT fft;
+
+    String audioFilePath;
+
+   
+    public FA() {
+        this.audioFilePath = "java/data/Heartbeat.mp3"; 
+    }
+
+
+    public FA(String audioFilePath) {
+        this.audioFilePath = audioFilePath;
+    }
+
+    public void settings() {
+        size(800, 600);
+    }
+
+    public void setup() {
+        minim = new Minim(this);
+        player = minim.loadFile(audioFilePath, 1024);
+        player.play();
+        fft = new FFT(player.bufferSize(), player.sampleRate());
+    }
+
+    public void draw() {
+        background(0);
+    }
+
+    public static void main(String[] args) {
+        String[] a = { "FA" };
+        PApplet.runSketch(a, new FA());
+    }
 }
