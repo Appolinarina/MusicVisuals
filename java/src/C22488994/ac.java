f8cab0ebd74f2ecebbfe7102f3ca7b6fdc574721
Apry-dev <C22488994@mytudublin.ie>
2024-04-18 17:47:28 +0100

    got Game of Life working. made it update the board with every beat

--- java/src/C22488994/ac.java
+++ java/src/C22488994/ac.java
@@ -1,42 +1,44 @@
 package C22488994;
 
-import ie.tudublin.Visual;
-
-import ddf.minim.AudioBuffer;
-import ddf.minim.AudioInput;
-import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
-import ddf.minim.ugens.Waveform;
+import ddf.minim.analysis.BeatDetect;
 import processing.core.PApplet;
+import ddf.minim.*;
 
+public class ac extends PApplet {
+    Minim minim;
+    BeatDetect beat;
+    LifeBoard board;
+    boolean beatDetected = false;
+    AudioPlayer player;
 
-public class ac extends PApplet{
-    // Your code here
-    Waveform wf;
-    PApplet p;
-    Minim m;
-    Visual visual; //declare visual object
-    
-    Minim minim; // declare minim object
-    
-    public void settings(){
-        //window size
-        size(width/10, height/10);
+    public void settings() {
+        size(1000, 1000);
+        board = new LifeBoard(200, this);
     }
 
-    
-    public void setup(){
-        //setup code
+    public void setup() {
         minim = new Minim(this);
-        visual = new Visual() {};
-        String fname = "Heartbeat.mp3";
-        m.loadFile(fname);
-        background(0);
+        beat = new BeatDetect();
+        player = minim.loadFile("Heartbeat.mp3");
+        player.play();
+        beat.setSensitivity(300); // Adjust sensitivity for beat detection
     }
-    
+
+    public void draw() {
+        background(0);
+        beat.detect(player.mix);
+        if (beat.isOnset()) {
+            beatDetected = true;
+        }
+        if (beatDetected) {
+            board.update();
+            beatDetected = false;
+        }
+        board.render();
+    }
+
     public static void main(String[] args) {
-        // ac ac = new ac();
-        // ac.setup();
         PApplet.main("C22488994.ac");
     }
 }
