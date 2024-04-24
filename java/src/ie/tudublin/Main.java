2cd986dd16cd74eea594a8923fd889d70d60eac6
Apry-dev <C22488994@mytudublin.ie>
2024-04-24 09:29:47 +0100

    added broken control panel to stagemanager.java

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -5,17 +5,29 @@ import C22488994.ac;
 import C22305656.FA;
 import ddf.minim.Minim;
 import ie.tudublin.Visual;
-public class Main{
-    public void startUI() {
-        String[] a = { "MAIN" };
-        //processing.core.PApplet.runSketch(a, new PP());
-        //processing.core.PApplet.runSketch(a, new FA());
-        processing.core.PApplet.runSketch(a, new ac());
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    StageManager stageManager;
+
+    public void settings() {
+        size(800, 600);
+    }
+
+    public void setup() {
+        stageManager = new StageManager(this);
+    }
+
+    public void draw() {
+        background(0);
+        stageManager.display();
+    }
+
+    public void keyPressed() {
+        stageManager.checkKeyPress(key);
     }
 
     public static void main(String[] args) {
-        Main main = new Main();
-        main.startUI();
-        
+        PApplet.main("ie.tudublin.Main");
     }
 }
--- java/src/ie/tudublin/StageManager.java
+++ java/src/ie/tudublin/StageManager.java
@@ -4,7 +4,39 @@ import processing.core.PApplet;
 import C22488994.ac;
 import C22447674.PP;
 import C22305656.FA;
+import C22304291.EC;
 
 public class StageManager {
-    
+    PApplet parent;
+
+    public StageManager(PApplet parent) {
+        this.parent = parent;
+    }
+
+    public void display() {
+        parent.textSize(20);
+        parent.fill(255);
+        parent.text("Press keys 1 to 4 to switch between sketches:", 10, 30);
+        parent.text("1: FA (Frequency Analysis)", 30, 60);
+        parent.text("2: EC (Heartbeat Visualization)", 30, 90);
+        parent.text("3: PP (Particle Playground)", 30, 120);
+        parent.text("4: AC (Game of Life)", 30, 150);
+    }
+
+    public void checkKeyPress(char key) {
+        switch (key) {
+            case '1':
+                PApplet.runSketch(new String[] { "FA" }, new FA());
+                break;
+            case '2':
+                PApplet.runSketch(new String[] { "EC" }, new EC());
+                break;
+            case '3':
+                PApplet.runSketch(new String[] { "PP" }, new PP());
+                break;
+            case '4':
+                PApplet.runSketch(new String[] { "AC" }, new ac());
+                break;
+        }
+    }
 }
